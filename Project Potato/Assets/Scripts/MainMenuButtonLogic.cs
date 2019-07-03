204d0adf1f22775e08da23d34906f426ba7fc36a
mzolfo <32077843+mzolfo@users.noreply.github.com>
2019-07-02 23:36:18 -0500

    basics of a main menu

---
+++ Project Potato/Assets/Scripts/MainMenuButtonLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonLogic : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject MainMenuScreen;
+    [SerializeField]
+    private GameObject CreditsScreen;
+
+   public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+   public void CreditsButton()
+    {
+        MainMenuScreen.SetActive(false);
+        CreditsScreen.SetActive(true);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void CreditsBackButton()
+    {
+        MainMenuScreen.SetActive(true);
+        CreditsScreen.SetActive(false);
+    }
+}
+
